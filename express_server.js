34ccb1066dae230460b386bd21db4f16ce460de9
PeterDevelops <PeterDinhBC@gmail.com>
2024-02-29 15:07:11 -0800

    Added routing for short urls

--- express_server.js
+++ express_server.js
@@ -26,6 +26,11 @@ app.get('/hello', (req, res) => { // sub domain
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 })
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id , longURL: urlDatabase.id };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => { // what port we
   console.log(`Example app listening on port: ${PORT}`);
 });
