bb733510c570e9e21e55953361b6cc68de0cc1d0
PeterDevelops <PeterDinhBC@gmail.com>
2024-02-29 16:20:57 -0800

    Changed the long url to take a bracket notation

--- express_server.js
+++ express_server.js
@@ -27,7 +27,7 @@ app.get('/hello', (req, res) => { // sub domain
 })
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id , longURL: urlDatabase.id };
+  const templateVars = { id: req.params.id , longURL: urlDatabase['id'] };
   res.render("urls_show", templateVars);
 });
 
