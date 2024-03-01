9b8760cbc40ab544ca2fbbe6ff573c81be8b930d
PeterDevelops <PeterDinhBC@gmail.com>
2024-02-29 17:57:43 -0800

    Added a post request

--- express_server.js
+++ express_server.js
@@ -20,6 +20,11 @@ app.get('/urls', (req, res) => { // subdomain /urls has access to the urls: urlD
   res.render('urls_index', templateVars);
 })
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get('/urls.json', (req, res) => { // sub domain
   res.json(urlDatabase);
 })
