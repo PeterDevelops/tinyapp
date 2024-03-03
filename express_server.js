dda4ef410e3c9dd0340b7bcd9cbae651fdc47b71
PeterDevelops <PeterDinhBC@gmail.com>
2024-03-02 20:23:23 -0800

    Added /logout post request

--- express_server.js
+++ express_server.js
@@ -58,6 +58,11 @@ app.post('/login', (req, res) => {
   res.redirect('/urls')
 });
 
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
 app.get('/urls.json', (req, res) => { // sub domain
   res.json(urlDatabase);
 });
