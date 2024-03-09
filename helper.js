3b9dfe07b84a8ddf7c1be8943422bdd0c333550b
PeterDevelops <PeterDinhBC@gmail.com>
2024-03-08 16:29:17 -0800

    Added a helper function module and test module

--- helper.js
+++ helper.js
@@ -1,10 +1,9 @@
-
 const getUserByEmail = (email, database) => {
   for (const id in database) {
     if (database[id].email === email) {
-      return database[id].id;
+      return database[id].id; // database[id].id
     }
   }
   return false;
-}
+};
 module.exports = { getUserByEmail };
